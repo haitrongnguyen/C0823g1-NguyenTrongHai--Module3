83f40a9df2262868c826d858787b72d9bef9cada
haitrongnguyen <tronghaispt@gmail.com>
2023-11-15 13:45:47 +0700

    nop bai tap bai 1

--- ss01_db/bai_1.sql
+++ ss01_db/bai_1.sql
@@ -1,12 +1,22 @@
 create database `student_management`;
 use student_management;
 CREATE TABLE class (
-    id INT,
+    class_id INT primary key auto_increment,
     `name` VARCHAR(50)
 );
+insert into class(name)
+values ('C0823G1');
 CREATE TABLE teacher (
-    id INT,
+    teacher_id INT primary key auto_increment,
     `name` VARCHAR(50),
     age INT,
-    country VARCHAR(50)
+    country VARCHAR(50),
+    class_id int,
+    foreign key(class_id) references class(class_id)
 );
+insert into teacher(name,age,country,class_id)
+values ('Jame',28,'Quảng Trị',1),
+('Alan',30,'Huế',1),
+('Walker',32,'Đà Nẵng',1);
+select *
+from teacher;
