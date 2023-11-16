4a5e03842d13966a65e3ff8fec7135e80455ff31
haitrongnguyen <tronghaispt@gmail.com>
2023-11-16 13:30:11 +0700

    sua bai tap bai 1

--- ss01_db/bai_1.sql
+++ ss01_db/bai_1.sql
@@ -1,11 +1,15 @@
-create database `student_management`;
-use student_management;
+CREATE DATABASE `student_management`;
+
+USE student_management;
+
 CREATE TABLE class (
     class_id INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(50)
 );
-insert into class(name)
-values ('C0823G1');
+
+INSERT INTO class(name)
+VALUES ('C0823G1');
+
 CREATE TABLE teacher (
     teacher_id INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(50),
@@ -15,10 +19,12 @@ CREATE TABLE teacher (
     FOREIGN KEY (class_id)
         REFERENCES class (class_id)
 );
-insert into teacher(name,age,country,class_id)
-values ('Jame',28,'Quảng Trị',1),
+
+INSERT INTO teacher(name,age,country,class_id)
+VALUES ('Jame',28,'Quảng Trị',1),
 ('Alan',30,'Huế',1),
 ('Walker',32,'Đà Nẵng',1);
+
 SELECT 
     *
 FROM
