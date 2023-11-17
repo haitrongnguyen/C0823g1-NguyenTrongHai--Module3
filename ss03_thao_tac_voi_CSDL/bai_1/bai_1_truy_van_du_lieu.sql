4d779ce53a9b0434d0b2cf5427da3caa3990094e
haitrongnguyen <tronghaispt@gmail.com>
2023-11-17 17:11:55 +0700

    nop bai tap bai 3

--- ss03_thao_tac_voi_CSDL/bai_1/bai_1_truy_van_du_lieu.sql
+++ ss03_thao_tac_voi_CSDL/bai_1/bai_1_truy_van_du_lieu.sql
@@ -8,7 +8,8 @@ where substring(start_date,6,2) = '12';
 
 select *
 from subject
-where credit >= 3 and credit <= 5;
+where credit >= 3 
+and credit <= 5;
 
 set sql_safe_updates = 0;
 update student
@@ -17,7 +18,9 @@ where student_name = 'Hung';
 set sql_safe_updates = 1;
 
 select student_name, sub_name, mask
-from mask join student join subject
-where mask.sub_id = subject.sub_id and mask.student_id = student.student_id
+from mask join student 
+join `subject`
+where mask.sub_id = subject.sub_id 
+and mask.student_id = student.student_id
 order by mask desc;
 
