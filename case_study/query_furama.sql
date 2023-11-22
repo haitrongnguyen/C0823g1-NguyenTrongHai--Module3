e9060fc7cb5fadcd0946fd0cd5e22e159038ed84
haitrongnguyen <tronghaispt@gmail.com>
2023-11-22 07:19:02 +0700

    nop bai case study

--- case_study/query_furama.sql
+++ case_study/query_furama.sql
@@ -129,11 +129,11 @@ group by hop_dong.ma_hop_dong;
 -- task 11.	Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
 select dich_vu_di_kem.ma_dich_vu_di_kem, dich_vu_di_kem.ten_dich_vu_di_kem
 from dich_vu_di_kem
-left join hop_dong_chi_tiet
+right join hop_dong_chi_tiet
 on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
-left join hop_dong
+join hop_dong
 on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
-left join khach_hang
+join khach_hang
 on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
 left join loai_khach
 on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
@@ -145,7 +145,7 @@ or khach_hang.dia_chi like '% Quảng Ngãi';
 -- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 
 -- nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
 select hop_dong.ma_hop_dong, nhan_vien.ho_ten, khach_hang.ho_ten, khach_hang.so_dien_thoai,
-dich_vu.ten_dich_vu, hop_dong.tien_dat_coc, sum(hop_dong_chi_tiet.so_luong)
+dich_vu.ten_dich_vu, hop_dong.tien_dat_coc, sum(ifnull(hop_dong_chi_tiet.so_luong,0))
 from hop_dong
 left join nhan_vien
 on nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien
@@ -157,8 +157,9 @@ left join hop_dong_chi_tiet
 on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
 where month(hop_dong.ngay_lam_hop_dong) in (10,11,12)
 and year(hop_dong.ngay_lam_hop_dong) = 2020
-and month(hop_dong.ngay_lam_hop_dong) not in (1,2,3,4,5,6)
-and year(hop_dong.ngay_lam_hop_dong) <> 2021
+and hop_dong.ngay_lam_hop_dong not between '20210101' and '20210630'
+-- month(hop_dong.ngay_lam_hop_dong) not in (1,2,3,4,5,6)
+-- and year(hop_dong.ngay_lam_hop_dong) <> 2021
 group by hop_dong.ma_hop_dong;
 
 -- task 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
@@ -211,14 +212,13 @@ having count(hop_dong.ma_nhan_vien) <= 3;
 -- task 16	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019 đến năm 2021.
 SET sql_safe_updates = 0;
 SET sql_safe_updates = 1;
-select *
-from nhan_vien
+
+delete from nhan_vien
 where nhan_vien.ma_nhan_vien not in
-(select nhan_vien.ma_nhan_vien
-from nhan_vien
-join hop_dong
-on nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien
+(select hop_dong.ma_nhan_vien
+from hop_dong
 where year(hop_dong.ngay_lam_hop_dong) between 2019 and 2021);
+
 
 -- task 17.	Cập nhật thông tin những khách hàng có ten_loai_khach từ Platinum lên Diamond, chỉ cập nhật những khách hàng đã từng đặt phòng với 
 -- Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ
@@ -228,21 +228,62 @@ sql_mode = 0;
 SET 
 sql_mode = 1;
 
-select khach_hang.ma_khach_hang,khach_hang.ho_ten,loai_khach.ten_loai_khach,sum(dich_vu.chi_phi_thue + hop_dong_chi_tiet.so_luong*dich_vu_di_kem.gia) as 'tong tien'
+select khach_hang.ma_khach_hang,khach_hang.ho_ten,loai_khach.ten_loai_khach,sum(dich_vu.chi_phi_thue + hop_dong_chi_tiet.so_luong*dich_vu_di_kem.gia)
 from khach_hang
 left join loai_khach
 on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
-join hop_dong
+left join hop_dong
 on hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
-join dich_vu
+left join dich_vu
 on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
-join hop_dong_chi_tiet
+left join hop_dong_chi_tiet
 on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
-join dich_vu_di_kem
+left join dich_vu_di_kem
 on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
 where year(hop_dong.ngay_lam_hop_dong) = 2021 
--- group by khach_hang.ma_khach_hang
--- having 'tong tien' >= 10000000;
+and loai_khach.ten_loai_khach = 'Platinium'
+group by khach_hang.ma_khach_hang
+having sum(dich_vu.chi_phi_thue + hop_dong_chi_tiet.so_luong*dich_vu_di_kem.gia) > 10000000;
+
+-- task 18.	Xóa những khách hàng có hợp đồng trước năm 2021 (chú ý ràng buộc giữa các bảng).
+
+SET SQL_SAFE_UPDATES=0;
+SET SQL_SAFE_UPDATES=1;
+SET FOREIGN_KEY_CHECKS=0; 
+SET FOREIGN_KEY_CHECKS=1; 
+
+delete from khach_hang
+where khach_hang.ma_khach_hang in
+(select hop_dong.ma_khach_hang
+from hop_dong
+where year(hop_dong.ngay_lam_hop_dong) < 2021);
+
+-- task 19.	Cập nhật giá cho các dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2020 lên gấp đôi.
+update dich_vu_di_kem
+set dich_vu_di_kem.gia = dich_vu_di_kem.gia*2
+where dich_vu_di_kem.ma_dich_vu_di_kem in
+(select hop_dong_chi_tiet.ma_dich_vu_di_kem
+from hop_dong_chi_tiet
+group by hop_dong_chi_tiet.ma_dich_vu_di_kem
+having sum(hop_dong_chi_tiet.so_luong) > 10);
+
+-- 20.	Hiển thị thông tin của tất cả các nhân viên và khách hàng có trong hệ thống, thông tin hiển thị bao gồm id (ma_nhan_vien, ma_khach_hang), 
+-- ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi.
+select nhan_vien.ma_nhan_vien,
+nhan_vien.ho_ten,
+nhan_vien.email,
+nhan_vien.so_dien_thoai,
+nhan_vien.ngay_sinh,
+nhan_vien.dia_chi
+from nhan_vien
+union
+select khach_hang.ma_khach_hang,
+khach_hang.ho_ten,
+khach_hang.email,
+khach_hang.so_dien_thoai,
+khach_hang.ngay_sinh,
+khach_hang.dia_chi
+from khach_hang;
 
 
 
